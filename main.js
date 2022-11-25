04f548ab4257bf82acf65b90d9031919ba921f19
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2022-11-25 23:14:29 +0100

    fix

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "11/25/",
+      dayMonth = "03/25/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
