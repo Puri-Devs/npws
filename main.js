c9c1b9eddbe4fbab54c82c575890c3e9a858906d
Mateusz Czarnecki <mateuszczarnecki360@gmail.com>
2022-11-25 23:13:55 +0100

    fix

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "03/25/",
+      dayMonth = "11/25/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
@@ -33,7 +33,7 @@
 
         //do something later when date is reached
         if (distance < 0) {
-          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("headline").innerText = "website is open!";
           document.getElementById("countdown").style.display = "none";
           document.getElementById("content").style.display = "block";
           clearInterval(x);
